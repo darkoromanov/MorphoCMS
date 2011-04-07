0eb729651bc01dd898a948b53036e820a9c90c44
darkoromanov <darko.romanov@gmail.com>
2011-04-08 01:11:30 +0200

    Entity edit page

---
+++ page_parts/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+ "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+  	<title>Morpho CMS</title>
+  	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  	<link rel="shortcut icon" href="/favicon.ico" >
+  	<link rel="stylesheet" href="/css/reset-fonts-grids.css" type="text/css">
+	<link rel="stylesheet" href="/css/morpho.css">
+	<link rel="stylesheet" href="/css/Aristo/jquery-ui-1.8.7.custom.css" >
+</head>
+<body>
+<div id="doc4" class="yui-t2">
+   <div id="hd">
+   	<a href="/" id="logo">Morpho</a>
+   	<ul id="top-menu">
+   		<li class="entities">
+   			<a href="/entities">Entities</a>
+   		</li>
+   		<li class="media">
+   			<a href="#">Media library</a>
+   		</li>
+   		<li class="folders">
+   			<a href="#">Folders</a>
+   		</li>
+   		<li class="users">
+   			<a href="#">Users</a>
+   		</li>
+   	</ul>
+   </div>
+   <div id="bd">
